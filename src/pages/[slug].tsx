7aab01e9597b1ddd2e7102d1856358ce88389ab4
Nahuel Luca <nahuelluca20@gmail.com>
2023-07-05 09:13:58 -0300

    add: profile page ui

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,6 +4,7 @@ import {type NextPage} from "next";
 import Head from "next/head";
 import {createServerSideHelpers} from "@trpc/react-query/server";
 import superjson from "superjson";
+import Image from "next/image";
 
 import {api} from "~/utils/api";
 import {prisma} from "~/server/db";
@@ -23,8 +24,18 @@ const ProfilePage: NextPage<{username: string}> = ({username}) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>Profile View</div>
-        <div>{data.username}</div>
+        <div className="relative h-48  bg-slate-600">
+          <Image
+            alt={`${data.username ?? ""}´s profile picture`}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+            height={128}
+            src={data.profileImageUrl}
+            width={128}
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
